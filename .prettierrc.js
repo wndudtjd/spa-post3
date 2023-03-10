ed63d61b1e9de4437969455af1b561525d9a8aaa
wndudtjd <dudtjdwn1575@naver.com>
2023-03-10 19:25:40 +0900

    prettier 설정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   printWidth: 80, //  줄 바꿈 할 폭 길이
   tabWidth: 2, // 탭 너비
+  semi: true, // 세미콜론 사용 여부
   singleQuote: true, // single 쿼테이션 사용 여부
   trailingComma: 'all', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
-}
+  arrowParens: trun,
+};
